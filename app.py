02045c806b19227697bbf8b5579d6957e3f602f5
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-12-29 15:49:56 +0000

    Commit

--- app.py
+++ app.py
@@ -2,69 +2,20 @@ from datetime import datetime
 import os
 import pyodbc
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from aubssc import loadUserFromDMCP
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    userId = request.headers.get('X-Ms-Client-Principal-Name')
-    user = {}
-
-    conn = pyodbc.connect(os.environ['DMCP_CONNECT_STRING'])
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM ssc.loggedInEmployee WHERE userName='" + userId + "';")
-    row = cursor.fetchone()
-
-    cursor.close()
-    conn.close()
-
-    user['firstName'] = row[0]
-    user['lastName'] = row[1]
-    user['fullName'] = row[2]
-    user['emailAddress'] = row[3]
-    user['locId'] = row[4]
-    user['locName'] = row[5]
-
+    
+    user = loadUserFromDMCP()
     return render_template('old-ssc/index.html', user=user)
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-    name = request.form.get('name')
-    userId = request.headers.get('X-Ms-Client-Principal-Name')
-
-    #for testing only
-    if userId is None:
-        userId='ghouser@aubreys.group'
-
-    user = {}
-
-    conn = pyodbc.connect(os.environ['DMCP_CONNECT_STRING'])
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM ssc.loggedInEmployee WHERE userName='" + userId + "';")
-    row = cursor.fetchone()
-
-    cursor.close()
-    conn.close()
-
-    user['firstName'] = row[0]
-    user['lastName'] = row[1]
-    user['fullName'] = row[2]
-    user['emailAddress'] = row[3]
-    user['locId'] = row[4]
-    user['locName'] = row[5]
-
-    if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name, user = user)
-    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
    app.run()
