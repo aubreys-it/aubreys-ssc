5fc10ada4fd80b119edbdd02f62245afb6ab62f6
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-12-29 22:00:47 +0000

    Commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-DMCP_CONNECT_STRING = os.environ(['DMCP_CONNECT_STRING'])
+DMCP_CONNECT_STRING = os.environ['DMCP_CONNECT_STRING']
 API_BASE_URI = 'https://ssc-api.azurewebsites.net/api/'
 
-GET_EMP_CODE = os.environ(['GET_EMP_CODE'])
+GET_EMP_CODE = os.environ['GET_EMP_CODE']
