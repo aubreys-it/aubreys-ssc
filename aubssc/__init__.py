eef979b852cd0b592deea9281f631b082234e543
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-12-29 15:49:42 +0000

    Commit

---
+++ aubssc/__init__.py
@@ -0,0 +1,31 @@
+import os
+import pyodbc
+from flask import request
+
+class user:
+    def __init__(self, userInfo):
+        self.firstName = userInfo[0]
+        self.lastName = userInfo[1]
+        self.fullName = userInfo[2]
+        self.emailAddress = userInfo[3]
+        self.locId = userInfo[4]
+        self.locName = userInfo[5]
+
+class db:
+    def __init__(self):
+        conn = pyodbc.connect(os.environ['DMCP_CONNECT_STRING'])
+        return conn.cursor()
+
+    def close(self):
+        self.close()
+
+def loadUserFromDMCP():
+    userId = request.headers.get('X-Ms-Client-Principal-Name')
+    sql = "SELECT * FROM ssc.loggedInEmployee WHERE userName='" + userId + "';"
+
+    dmcp = db()
+    dmcp.execute(sql)
+    userRecord = dmcp.fetchone()
+    dmcp.close()
+
+    return user(userRecord).__dict__
