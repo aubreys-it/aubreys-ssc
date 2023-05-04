eadbed5f96d1a913c2a8e7fa9a8f1c991642c6f5
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2023-05-04 12:35:05 +0000

    Multiple Must Calls All Locations

--- app.py
+++ app.py
@@ -112,13 +112,13 @@ def settings():
             else:
                 postDict['autoRotateBar'] = 0
 
-            if user['locId'] in [0, 13, 14, 23]:
-                postDict['mustCallNeeded'] = request.form['mustCallNeeded']
-            else:
-                if request.form.get('mustCallNeeded'):
-                    postDict['mustCallNeeded'] = 1
-                else:
-                    postDict['mustCallNeeded'] = 0
+            #if user['locId'] in [0, 13, 14, 23]:
+            postDict['mustCallNeeded'] = request.form['mustCallNeeded']
+            #else:
+            #    if request.form.get('mustCallNeeded'):
+            #        postDict['mustCallNeeded'] = 1
+            #    else:
+            #        postDict['mustCallNeeded'] = 0
 
 
             result = updateSettings(postDict)
